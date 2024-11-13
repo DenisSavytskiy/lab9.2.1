3eb67320904bf6463b97a0bcb2da95ded02745c5
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-11-14 01:34:37 +0200

    first commit

---
+++ AP_Lab№9.2.1/AP_Lab№9.2.1.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+
+#define Z 4
+#define SQR(x) ((x)*(x))
+#define MAX(y,z) ((y)>(z))?(y):(z)
+#define PRINTI(w) puts("control output"); \
+    printf(#w"=%d\n",w)
+#define PRINTR(w) puts ("rezult :"); \
+    printf (#w"=%f\n",(float)w)
+
+#if Z>3
+#define CALC_W(x, y) MAX(SQR(x - Z), x - y)
+#else
+#define CALC_W(x, y) (MAX(x* y, y* Z) + MAX(Z * x, y * Z))
+#endif
+
+void main()
+{
+    puts("Lab 9.2. Using macros and preprocessing directive");
+    int x, y;
+    float w;
+    char ch;
+
+    do
+    {
+        puts("Input x, y, z:");
+        scanf_s("%d%d%d", &x, &y);
+        PRINTI(x);
+        PRINTI(y);
+
+        w = CALC_W(x, y);
+        PRINTR(w);
+
+        puts("Repeat? y/n ");
+        ch = _getch();
+    } while (ch == 'y');
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№9.2.1/AP_Lab№9.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestSQR)
+        {
+            int x = 4;
+            int result = SQR(x);
+            Assert::AreEqual(16, result);
+        }
+
+        TEST_METHOD(TestMAX)
+        {
+            int a = 5, b = 3;
+            int result = MAX(a, b);
+            Assert::AreEqual(5, result);
+        }
+    };
+}
